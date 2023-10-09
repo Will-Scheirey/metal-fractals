63c8b27e3600b7e59a9e725490f9462ffc336de2
Will Scheirey <williamscheirey@gmail.com>
2023-10-09 12:20:56 -0400

    Initial Commit

---
+++ shaders/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  shaders
+//
+//  Created by William Scheirey on 10/9/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ shaders/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  shaders
+//
+//  Created by William Scheirey on 10/9/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
